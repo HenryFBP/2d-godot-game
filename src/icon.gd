9a7922d01e5798a51c4f37e50c73af6ca0a7fed3
Henry Post <HenryFBP@gmail.com>
2018-07-27 16:04:48 -0400

    add collision to ground

--- src/icon.gd
+++ src/icon.gd
@@ -3,22 +3,22 @@ extends KinematicBody2D
 var speed = 250
 var velocity = Vector2()
 
+var movement_factor = 1
+var input_maps = {
+	'player_right': Vector2(movement_factor, 0),
+	'player_left': Vector2(-movement_factor, 0),
+	'player_up': Vector2(0, -movement_factor),
+	'player_down': Vector2(0, movement_factor),
+	}
+
 func get_input():
-	var factor = 10
-	# Detect up/down/left/right keystate and only move when pressed
+
 	velocity = Vector2()
 	
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += factor
-
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= factor
-
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += factor
-
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= factor
+	for key in input_maps.keys():
+		if Input.is_action_pressed(key):
+			velocity += input_maps[key]
+	
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
