b6e4be39d4065720970e2a4348cce4279570b22f
HenryFBP <HenryFBP@gmail.com>
2018-07-30 20:01:50 -0400

    tweak method setup, compartmentalize drawing

--- src/CanvasLayer.gd
+++ src/CanvasLayer.gd
@@ -15,16 +15,15 @@ func _ready():
 	# Initialization here
 	pass
 	
-func draw_circle_arc(center, radius, angle_from, angle_to, color):
+func set_draw_circle_arc(center, radius, angle_from, angle_to, color):
 	self.center = center
 	self.radius = radius
 	self.angle_from = angle_from
 	self.angle_to = angle_to
 	self.color = color
 
-
-func _draw():
-	
+func draw_circle_arc():
+		
     var nb_points = 32
     var points_arc = PoolVector2Array()
 
@@ -35,6 +34,8 @@ func _draw():
     for index_point in range(nb_points):
         draw_line(points_arc[index_point], points_arc[index_point + 1], color)	
 
+func _draw():
+	draw_circle_arc()
 
 func _process(delta):
 	update()
