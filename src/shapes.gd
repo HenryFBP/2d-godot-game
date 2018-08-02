780f5675012c5ab963a30cabdf8582dcc7b8c1d0
Henry Post <HenryFBP@gmail.com>
2018-08-02 18:23:14 -0400

    remove unnecessary shapes.gd

--- src/shapes.gd
+++
@@ -1,9 +0,0 @@
-func points_to_convexpolygonshape2d(points):
-
-	var shape = ConvexPolygonShape2D.new()
-	
-	
-	
-	var pc = PoolVector2Array([ Vector2(0.0, 50.0), Vector2(0.0, 0.0), Vector2(50.0, 0.0), Vector2(50.0, 50.0) ])
-	
-	shape.set_point_cloud(pc)
